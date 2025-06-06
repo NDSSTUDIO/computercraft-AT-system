a1c63f9bcc095d3dc609c7cca5d31d649923c1c6
17ik0 <98870938+NDSSTUDIO@users.noreply.github.com>
2025-06-06 09:27:55 +0800

    Update startup.lua

--- startup.lua
+++ startup.lua
@@ -29,22 +29,33 @@ end
 monitor.clear()
 monitor.setTextScale(0.5)
 local hud_scale = monitor.getSize()
+local hud_type = 0 -- 为自定义hud做准备（）
 
 -- 初始化变速箱
-local gear = 0
-local speed = 0
+gear = 0
+speed = 0
+gearbox_type = 2
+-- 0 = MT
+-- 1 = auto_snug
+-- 2 = auto_sports
 
 -- 初始化引擎
-local engine_speed = 8 -- 启动转速
+local engine_speed = 10 -- 启动转速
 local maxSpeed = 80 -- 最大转速
 local acceleration = 3 -- 加速率
 local deceleration = 2 -- 减速率
-local throttle = 0.0 -- 油门
+throttle = 0.0 -- 油门
 local load = 0 -- 负载(负载=车重*变速箱档位)
 local mass = 0.6 -- 车重(负载基数)
 
 -- HUD
-local function hud(speed,gear,rpm,relspeed)
+function hud(speed,gear,rpm,relspeed)
+    if hud_type == 0 then
+        hud_0(speed,gear,rpm,relspeed)
+    end
+end
+
+function hud_0(speed,gear,rpm,relspeed)
     monitor.clear()
     monitor.setTextScale(0.5)
     monitor.setCursorPos(1,1)
@@ -68,9 +79,9 @@ local function hud(speed,gear,rpm,relspeed)
     -- 转速表
     monitor.setCursorPos(1,(hud_scale / 2) + 2)
     monitor.write("-----------------------------------------------------------")
-    monitor.setCursorPos((hud_scale / 2 - 5) - rpmstr*5,(hud_scale / 2) + 2)
+    monitor.setCursorPos(((hud_scale / 2 - 5) - rpmstr*5) + 2,(hud_scale / 2) + 2)
     monitor.write("+----+----+----+----+----+----+----+----+----+----+----+---------")
-    monitor.setCursorPos((hud_scale / 2 - 5) - rpmstr*5,(hud_scale / 2) + 3)
+    monitor.setCursorPos(((hud_scale / 2 - 5) - rpmstr*5) + 2,(hud_scale / 2) + 3)
     monitor.write("     0    1    2    3    4    5    6    7    8    9   10         ")
     monitor.setCursorPos((hud_scale / 2) + 1,(hud_scale / 2) + 2)
     monitor.write("|")
@@ -106,31 +117,85 @@ local function hud(speed,gear,rpm,relspeed)
         monitor.setCursorPos((hud_scale / 2) + 7,2)
         monitor.write("=")
     end
+   
+    if gearbox_type == 1 then
+
+    elseif gearbox_type == 2 then
+    monitor.setCursorPos((hud_scale / 2) - 1,3)
+    monitor.write("SPORT")
+    end
 end
+
+-- 变速箱模式
+function gearbox(gearbox_type)
+    if gearbox_type == 2 then
+        auto_sports()
+    elseif gearbox_type == 1 then
+        auto_snug()
+    end
+end   
+
+-- 变速箱：AT运动模式
+function auto_sports()
+    maxSpeed = 80 -- 最大引擎转速:8000RPM
+    if speed < 2 then
+        gear = 1
+    end
+    if engine_speed > 75 then
+        gear = math.min(gear + 1,5)
+    elseif engine_speed < 30 then
+        gear = math.max(gear -1,1)
+    end  
+end
+
+-- 变速箱：AT舒适模式
+function auto_snug()
+    maxSpeed = 50 -- 最大引擎转速:5000RPM
+    if speed < 2 then
+        gear = 1
+    end
+    if engine_speed > 30 then
+        gear = math.min(gear + 1,5)
+    elseif engine_speed < 10 then
+        gear = math.max(gear -1,1)
+    end  
+end
+
+
 
 
 -- 主循环(有很多东西可以简化成funciton)
 while true do
-    local joy_y = joy.getAxis(2) -- 获取手柄参数
+    local joy_y = joy.getAxis(2) -- 获取手柄左摇杆Y轴
+    local joy_rt = joy.getAxis(6) -- 获取手柄右扳机
+    local joy_lt = joy.getAxis(5) -- 获取手柄左扳机
+
     local truespeed = ship.getVelocity() -- 获取速度分量
-    local relspeed = math.sqrt(truespeed.x^2 + truespeed.z^2) -- 计算真实速度(m/s)
+    relspeed = math.sqrt(truespeed.x^2 + truespeed.z^2) -- 计算真实速度(m/s)
 
     -- 引擎速度（轮速->齿轮比->引擎速度）
     if gear == 1 then -- AT1档
+        load = 1.2 * mass -- 负载
         engine_speed = speed / 1.7 -- 轮速
     elseif gear == 2 then -- AT2档 *2
+        load = 2.0 * mass -- 负载
         engine_speed = speed / (1.7 * 2)
     elseif gear == 3 then -- AT3档 *3
+        load = 2.5 * mass -- 负载
         engine_speed = speed / (1.7 * 3)
     elseif gear == 4 then -- AT4档 *4
+        load = 3 * mass -- 负载
         engine_speed = speed / (1.7 * 4)
     elseif gear == 5 then -- AT5档 *5
+        load = 4.5 * mass -- 负载
         engine_speed = speed / (1.7 * 5)
     elseif gear == 0 then
+        load = 0 * mass -- 负载
         engine_speed = engine_speed
     elseif gear == -1 then -- 倒挡
         if joy_y ~= -1  then -- 排除刹车
-        engine_speed = speed / -1.7 
+            load = 1.2 * mass -- 负载
+            engine_speed = speed / -1.7 
         end
     end
 
@@ -139,7 +204,7 @@ while true do
         engine_speed = math.min((engine_speed + acceleration * throttle) - load, maxSpeed)  -- 加速
         -- (引擎速度+加速率*油门开度)- 负载 小于最大速度
     else -- 减速
-        engine_speed = math.max(engine_speed - deceleration, 0)  
+        engine_speed = math.max(engine_speed - deceleration, 2)  
         -- 引擎速度-减速率 最小0rpm
     end
 
@@ -179,39 +244,29 @@ while true do
         gear = 0
     end
     
-    if joy_y < 0 then -- 油门：FULL
+    if joy_y < 0 or joy_rt > 0.1 then -- 油门
+        
         if gear == -1 then
                 -- 倒车时的刹车按键
                 -- 引擎减速率=5
                 deceleration = 5
-            else
-                deceleration = 2
-                maxSpeed = 80 -- 最大引擎转速:8000RPM
-                throttle = 1  -- 油门开度:100%
-            if relspeed < 3 then -- AT1档
-                load = 1.2 * mass -- 负载
-                gear = 1
-            elseif relspeed < 13 and relspeed >= 5 then -- AT2档  *2
-                load = 2.0 * mass -- 负载
-                gear = 2
-            elseif relspeed < 24 and relspeed >= 13 then -- AT3档 *3
-                load = 2.5 * mass -- 负载
-                gear = 3
-            elseif relspeed < 38 and relspeed >= 24 then -- AT4档 *4
-                load = 3 * mass -- 负载
-                gear = 4
-            elseif relspeed >= 38 then -- AT5档 *5
-                load = 4.5 * mass -- 负载
-                gear = 5
-            end
+        else
+                deceleration = 2 -- 引擎减速率=2
+                if joy_rt > 0.1 then
+                    throttle = joy_rt
+                else
+                    throttle = math.abs(joy_y)
+                end
+                gearbox(gearbox_type)
         end
+    elseif joy_y > 0 or joy_lt > 0.1 then -- 油门：倒挡/刹车
         
-    elseif joy_y == 0 then -- 油门：空
-        throttle = 0 -- 油门开度:0%
-        maxSpeed = 80 -- 最大引擎转速:8000RPM
-    elseif joy_y > 0  then 
-        -- 油门：倒挡/刹车
         if relspeed < 5 then -- <5m/s 倒挡
+            if joy_rt > 0.1 then
+                throttle = joy_rt
+            else
+                throttle = math.abs(joy_y)
+            end
             load = 1.5-- 负载1.0*重量
             throttle = 1 -- 油门开度:30%
             maxSpeed = 35 -- 最大引擎转速:3500RPM
@@ -222,6 +277,10 @@ while true do
             -- 引擎减速率=5
             deceleration = 5
         end
+    elseif joy_y == 0 then -- 油门：空
+        throttle = 0 -- 油门开度:0%
+        maxSpeed = 80 -- 最大引擎转速:8000RPM
+
     end
     speedcontroller.setTargetSpeed(speed) -- 将轮速更改实施
     
@@ -231,4 +290,3 @@ while true do
     -- hud渲染
     hud(speed,gear,rpm,relspeed)    
 end
-
