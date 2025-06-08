732d7b6bfdbbdd7a6dd4dcdea1965ed764dadcc4
17ik0 <98870938+NDSSTUDIO@users.noreply.github.com>
2025-06-08 22:02:22 +0800

    Update startup.lua

--- startup.lua
+++ startup.lua
@@ -1,68 +1,109 @@
--- startup.lua  版本2025/6/6 16:00
--- 转速控制器
-local speedcontroller = peripheral.find("Create_RotationSpeedController")
-if not speedcontroller then
-    print("NOT FOUND SPEEDCONTROLLER")
-    return
-end
+-- startup.lua  版本2025/6/8 21:24
+-- 修改初始配置请第一次启动后查看config.lua
+ver = "-- 1.0" 
+-- 配置文件版本请勿更改
 
+-- 配置文件
+configfile = "-- 1.0\n-- 配置文件版本请勿更改\n".."hud_type = 0 -- 为自定义hud做准备\nparking_brake = 1 -- 手刹\n-- 初始化变速箱\ngear = 0\nspeed = 0\ngearbox_type = 0\n-- 0 = MT\n-- 1 = auto_snug\n-- 2 = auto_sports\n-- 3 = auto_overload\n-- 初始化引擎\nengine_speed = 0 -- 启动转速\nmaxSpeed = 80 -- 最大转速（此处仅为启动时最大转速）\nacceleration = 3 -- 加速率（同上）\ndeceleration = 2 -- 减速率（同上）\nthrottle = 0.0 -- 油门\nload = 0 -- 负载(负载=车重*一个很迷的数？)\nmass = 0.6 -- 车重(负载基数)\nstartup = 1.6 -- 引擎启动参数 -1：待命 0.9：启动 1.6：执行启动动作\n-- 初始化其他参数\nrelspeed = 0 -- 真实速度（m/s）"
+
+
+--- 初始化
 -- 显示器
 local monitor = peripheral.find("monitor")
 if not monitor then
     print("NOT FOUND MONITOR")
 end
 
+-- 初始化HUD参数
+monitor.clear()
+monitor.setTextScale(0.5)
+monitor.setCursorPos(1,1)
+local hud_scale = monitor.getSize()
+
+
+-- 配置文件
+local config = io.open("config.lua","r")
+if config then
+    io.input(config)
+    configVer = (io.read())
+    print("ConfigVer:"..configVer)
+    if configVer ~= ver then
+        monitor.setCursorPos(1,1)
+        monitor.write("updConfig")
+        os.sleep(0.1)
+        local config = io.open("config.lua","w")
+        io.output(config)
+        io.write(configfile)
+        io.close(config)
+        monitor.setCursorPos(1,2)
+        monitor.write("reboot...")
+        os.sleep(1)
+        os.reboot()
+    end
+    monitor.setCursorPos(1,1)
+    monitor.write("configOK")
+    monitor.setCursorPos(1,1)
+    monitor.write(config)
+    dofile("config.lua")
+    io.close(config)
+else
+    monitor.setCursorPos(1,1)
+    monitor.write("createConfig")
+    os.sleep(0.1)
+    local config = io.open("config.lua","w")
+    io.close(config)
+    local config = io.open("config.lua","a")
+    io.output(config)
+    io.write(configfile)
+    io.close(config)
+    monitor.setCursorPos(1,1)
+    monitor.write("reboot...        ")
+    os.sleep(1)
+    os.reboot()
+end
+
+
+-- 转速控制器
+local speedcontroller = peripheral.find("Create_RotationSpeedController")
+if not speedcontroller then
+    monitor.setCursorPos(1,1)
+    monitor.write("NOT FOUND SPEEDCONTROLLER       ")
+    return
+end
+
 -- 放在书架上的Create:Tweaked Controller
 local joy = peripheral.find("tweaked_controller")
 if not joy then
-    print("NOT FOUND JOY")
+    monitor.setCursorPos(1,1)
+    monitor.write("NOT FOUND JOY                 ")
 end
 
 -- 扬声器
 local speaker = peripheral.find("speaker")
 if not speaker then
-    print("NOT FOUND SPEAKER")
+    monitor.setCursorPos(1,1)
+    monitor.write("NOT FOUND SPEAKER               ")
 end
 
------------- 初始化
--- 初始化HUD
-monitor.clear()
-monitor.setTextScale(0.5)
-local hud_scale = monitor.getSize()
-local hud_type = 0 -- 为自定义hud做准备（）
-
--- 初始化变速箱
-gear = 0
-speed = 0
-gearbox_type = 2
--- 0 = MT
--- 1 = auto_snug
--- 2 = auto_sports
--- 3 = auto_overload
-
--- 初始化引擎
-local engine_speed = 0 -- 启动转速
-local maxSpeed = 80 -- 最大转速
-local acceleration = 3 -- 加速率
-local deceleration = 2 -- 减速率
-throttle = 0.0 -- 油门
-local load = 0 -- 负载(负载=车重*一个很迷的数？)
-mass = 0.6 -- 车重(负载基数)
-
-startup = 1.5
-
--- startengine
+-- 启动引擎
 function startengine()
+    if startup == -1 then
+        local joy_start = joy.getButton(7)
+        if joy_start == true then
+            startup = 1.6
+        end
+    end
     if startup > 1 then
+        hud(speed,gear,rpm,relspeed)
         startup = math.max((startup - 0.05),1)
-        engine_speed = math.min((engine_speed + 5),30)
-        speaker.playNote("didgeridoo",(math.max(engine_speed / 26, 0.5)),(engine_speed / 8))
-        speaker.playNote("bass",(math.max(engine_speed / 26, 0.5)),(engine_speed / 5))
+        engine_speed = math.min((engine_speed + 5),12)
+        speaker.playNote("didgeridoo",(math.max(engine_speed / 26, 2)),(engine_speed / 3))
+        speaker.playNote("bit",(math.max(engine_speed / 26, 2)),(engine_speed / 2))
         os.sleep(0.15)
     elseif startup == 1 and startup > 0.9 then
-        speaker.playNote("didgeridoo",(math.max(engine_speed / 26, 1)),(engine_speed / 8))
+        speaker.playNote("didgeridoo",(math.max(engine_speed / 26, 3)),(engine_speed / 3))
         
-        engine_speed = 30
+        engine_speed = 15
         startup = 0.9
     end
 end
@@ -118,24 +159,48 @@ function hud_0(speed,gear,rpm,relspeed)
     monitor.write("=")
 
    
-    if gearbox_type == 1 then
-
+    if gearbox_type == 3 then
+    monitor.setCursorPos((hud_scale / 2) - 3,3)
+    monitor.write("OVERLOAD")
     elseif gearbox_type == 2 then
     monitor.setCursorPos((hud_scale / 2) - 1,3)
     monitor.write("SPORT")
     end
+    
+    
+    if parking_brake == 1 then
+        monitor.setCursorPos(1,3)
+        monitor.write("                     ")
+        monitor.setCursorPos((hud_scale / 2) - 1,3)
+        monitor.write("BREAK")
+    end
 end
 
--- 变速箱模式
+-- 变速箱模式（自动）
 function gearbox(gearbox_type)
-    if gearbox_type == 2 then
-        auto_sports()
-    elseif gearbox_type == 1 then
+    if gearbox_type == 1 then
         auto_snug()
+    elseif gearbox_type == 2 then
+        auto_sports()
     elseif gearbox_type == 3 then
         auto_overload()
     end
 end   
+
+
+-- 变速箱：MT手动
+function mt()
+    local joy_a = (joy.getButton(1)) -- 获取A按键（XBOX）
+    local joy_b = (joy.getButton(2)) -- 获取B按键（XBOX）
+    if joy_b == true then -- B加档
+        gear = math.min((gear + 1),5)
+        os.sleep(0.05)
+    end
+    if joy_a == true then -- A减档
+        gear = math.max((gear - 1),-1)
+        os.sleep(0.05)
+    end
+end
 
 -- 变速箱：AT运动模式
 
@@ -144,6 +209,10 @@ function auto_sports()
     acceleration = 3 -- 加速率
     deceleration = 2 -- 减速率
     maxSpeed = 80 -- 最大引擎转速:8000RPM
+    if relspeed < 0.3 then -- 如果真实速度小于0.3则自动空档
+        gear = 0
+    end
+
     if speed < 2 then
         gear = 1
     end
@@ -171,7 +240,7 @@ function auto_snug()
     end  
 end
 
--- 变速箱：过载
+-- 变速箱：AT过载
 function auto_overload()
     mass = 0.6
     acceleration = 3 -- 加速率
@@ -246,6 +315,7 @@ function engine_speed_ctl()
     end
 end
 
+-- 引擎油门（节气门）
 function engine_accelerator()
     if throttle > 0 then -- 加速
         engine_speed = math.min((engine_speed + acceleration * throttle) - load, maxSpeed)  -- 加速
@@ -256,42 +326,22 @@ function engine_accelerator()
     end
 end
 
-function main()
-        local joy_y = joy.getAxis(2) -- 获取手柄左摇杆Y轴
-    local joy_rt = joy.getAxis(6) -- 获取手柄右扳机
-    local joy_lt = joy.getAxis(5) -- 获取手柄左扳机
-
-    local truespeed = ship.getVelocity() -- 获取速度分量
-    relspeed = math.sqrt(truespeed.x^2 + truespeed.z^2) -- 计算真实速度(m/s)
-
-    -- 引擎速度（轮速->齿轮比->引擎速度）
-    engine_speed_ctl()
-    -- 引擎油门（节气门）
-    engine_accelerator()
-
-    -- 轮速度（引擎速度->齿轮比->轮速）
-    speed_ctl()
-
-    speedcontroller.setTargetSpeed(speed) -- 这东西不放这会出问题=-=
-
-    if relspeed < 0.3 then -- 如果真实速度小于0.3则自动空档
-        gear = 0
-    end
-    
+-- 控制器（前进/后退）（自动）
+function car_ctl_auto()
     if joy_y < 0 or joy_rt > 0.1 then -- 油门
         
-        if gear == -1 and relspeed > 3 then
-                -- 倒车时的刹车按键
-                -- 引擎减速率=5
-                deceleration = 5
+    if gear == -1 and relspeed > 3 then
+        -- 倒车时的刹车按键
+        -- 引擎减速率=5
+        deceleration = 5
         else
-                deceleration = 2 -- 引擎减速率=2
-                if joy_rt > 0.1 then
-                    throttle = joy_rt
-                else
-                    throttle = math.abs(joy_y)
-                end
-                gearbox(gearbox_type)
+            deceleration = 2 -- 引擎减速率=2
+            if joy_rt > 0.1 then
+                throttle = joy_rt
+            else
+                throttle = math.abs(joy_y)
+            end
+            gearbox(gearbox_type)   
         end
     elseif joy_y > 0 or joy_lt > 0.1 then -- 油门：倒挡/刹车
         
@@ -315,6 +365,103 @@ function main()
         throttle = 0 -- 油门开度:0%
         maxSpeed = 80 -- 最大引擎转速:8000RPM
     end
+end
+
+-- 控制器（油门/刹车/离合）（手动）
+function car_ctl_mt()
+    local joy_lb = (joy.getButton(5)) -- 获取L按键（XBOX）
+
+    if joy_y < 0 or joy_rt > 0.1 then -- 油门
+            deceleration = 2 -- 引擎减速率=2
+            if joy_rt > 0.1 then
+                throttle = joy_rt
+            else
+                throttle = math.abs(joy_y)
+            end     
+    elseif joy_y < 0.1 then
+        throttle = 0
+        -- 减速
+    elseif joy_y > 0 or joy_lb == true then -- 刹车
+        throttle = 0
+        -- 减速
+        brake = 1
+        speed = speed * (1 - brake) -- 减速
+    end
+    if joy_lt > 0.1 then -- 离合器
+        mt()
+    else
+        speed_ctl()
+        engine_speed_ctl()
+    end
+    engine_accelerator()
+end
+
+function main()
+    -- 通用
+    joy_add = joy.getButton(8) -- 获取+/菜单按键/开始（XBOX）
+    joy_x = joy.getButton(3) -- 获取X按键（XBOX）
+    joy_start = joy.getButton(7) -- 获取返回按键(XBOX)
+    joy_y = joy.getAxis(2) -- 获取手柄左摇杆Y轴
+    joy_rt = joy.getAxis(6) -- 获取手柄右扳机
+    joy_lt = joy.getAxis(5) -- 获取手柄左扳机
+
+
+    local truespeed = ship.getVelocity() -- 获取速度分量
+    relspeed = math.sqrt(truespeed.x^2 + truespeed.z^2) -- 计算真实速度(m/s)
+    
+    -- 关闭引擎
+    if joy_start == true then
+        startup = -1
+    end
+
+    -- 变速箱模式切换
+    if joy_add == true then
+        if gear ~= 5 then
+            engine_speed = 0
+            gearbox_type = gearbox_type + 1
+            if gearbox_type == 4 then
+                gearbox_type = 0
+            end
+        end
+        os.sleep(0.5)
+    end
+    
+    
+
+    -- 手刹
+    if joy_x == true then
+        if parking_brake == 1 then
+            parking_brake = 0
+            
+        else
+            parking_brake = 1
+        end
+        os.sleep(0.05)
+    end
+
+    if parking_brake == 1 then
+        speed = 0
+
+    end
+
+    -- 引擎速度（轮速->齿轮比->引擎速度）
+    if gearbox_type ~= 0 then
+        engine_speed_ctl()
+
+        -- 引擎油门（节气门）
+        engine_accelerator()
+
+        -- 轮速度（引擎速度->齿轮比->轮速）
+        speed_ctl()
+    end
+
+    speedcontroller.setTargetSpeed(speed) -- 这东西不放这会出问题=-=
+
+    if gearbox_type ~= 0 then
+        car_ctl_auto()
+    else
+        car_ctl_mt()
+    end
 
     speedcontroller.setTargetSpeed(speed) -- 将轮速更改实施
     
@@ -325,8 +472,13 @@ function main()
     hud(speed,gear,rpm,relspeed)    
 end
 
+
+
+
+
 -- 主循环(有很多东西可以简化成funciton)
 while true do
+    
     startengine()
     if startup == 0.9 then
         main()
